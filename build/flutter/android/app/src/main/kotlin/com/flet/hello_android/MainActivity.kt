f41b3dad48e53261fc79995f7f93543577f58f43
dusanrsc <dusanrosic25.06.1997@gmail.com>
2025-07-04 17:38:31 +0200

    Added Dark And Light Modes

---
+++ build/flutter/android/app/src/main/kotlin/com/flet/hello_android/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flet.hello_android
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- main.py
+++ main.py
@@ -11,12 +11,13 @@ NAVBAR_HEIGHT = 60
 GAP = 30
 
 WINDOW_TITLE = "Hello, Android!"
+DEFAULT_FONT_SIZE = 30
 
 # Hexadecimal Color CONSTANTS Section.
 # Basic Hexadecimal Color CONSTANTS Section.
-RED = "#FF0000"
+RED   = "#FF0000"
 GREEN = "#00FF00"
-BLUE = "#0000FF"
+BLUE  = "#0000FF"
 
 BLACK = "#000000"
 GRAY  = "#333333"
@@ -25,12 +26,46 @@ WHITE = "#FFFFFF"
 ALPHA = GREEN
 
 # Custom Hexadecimal Color CONSTANTS Section.
-MY_RED = "#FFAAAA"
+MY_RED   = "#FFAAAA"
 MY_GREEN = "#AAFF11"
-MY_BLUE = "#0A0AFF"
+MY_BLUE  = "#0A0AFF"
+
+# Default Hexadecimal CONSTANTS Colors Section.
+DEFAULT_PAGE_COLOR = GRAY
+DEFAULT_CONTAINER_COLOR = BLACK
+DEFAULT_TEXT_COLOR = MY_GREEN
+
+CHANGED_PAGE_COLOR = None
+CHANGED_CONTAINER_COLOR = WHITE
+CHANGED_FONT_COLOR = None
 
 # Main Program Logic Section.
 def main(page: Page):
+
+    # Local Program Functions Section.
+    # On Text Click Function.
+    def on_text_click(event):
+
+        # Condition For Switching Between Hello Android String Colors.
+        if hello_android.color == MY_GREEN:
+            hello_android.color = GREEN
+        else:
+            hello_android.color = MY_GREEN
+        
+        # Updating Changed Colors.
+        hello_android.update()
+
+    # Switch Between Modes Function.
+    def switch_dark_or_light_mode(event):
+
+        # Condition For Switching Between Two Modes.
+        if container.bgcolor != CHANGED_CONTAINER_COLOR:
+            container.bgcolor = CHANGED_CONTAINER_COLOR
+        else:
+            container.bgcolor = DEFAULT_CONTAINER_COLOR
+        
+        # Updating Changed Color Modes.
+        container.update()
 
     # Main Page Window Settings Section
     page.title = WINDOW_TITLE
@@ -40,16 +75,19 @@ def main(page: Page):
     page.bgcolor = GRAY
     page.update()
 
+    # Widgets Section.
+    hello_android = Text(color=DEFAULT_TEXT_COLOR, size=DEFAULT_FONT_SIZE, spans=[TextSpan(WINDOW_TITLE, on_click=on_text_click)])
+    switch_mode = Switch(on_change=switch_dark_or_light_mode)
+
     # Creating Container Widget.
     container = Container(
         width=WINDOW_WIDTH,
         height=WINDOW_HEIGHT,
-        bgcolor=BLACK,
+        bgcolor=DEFAULT_CONTAINER_COLOR,
         border_radius=35,
         alignment=alignment.center,
         
-        content=Column([
-            Text(f"{WINDOW_TITLE}", color=GREEN, size=30)],
+        content=Column([hello_android, switch_mode],
 
             # Align Content (Text) To The Center Of The Container(Screen)!
             alignment=MainAxisAlignment.CENTER,
